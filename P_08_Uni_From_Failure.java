cfbe0300dae49824d0c7c50e3e386ee0068c7704
JorgeDeLira <e-jedelira@neoris.com>
2025-04-29 16:53:43 -0700

    5

--- P_08_Uni_From_Failure.java
+++ P_08_Uni_From_Failure.java
@@ -1,4 +1,17 @@
 package P_01_basics;
 
+import io.smallrye.mutiny.Uni;
+
+import java.io.IOException;
+
 public class P_08_Uni_From_Failure {
+    public static void main(String[] args) {
+        System.out.println("Uni from failure");
+
+        Uni.createFrom().failure(new IOException("Boom"))
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+        Uni.createFrom().failure(() -> new IOException("Badaboom"))
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+    }
 }
