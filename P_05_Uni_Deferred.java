0d607e3984815f33961f5ca4a5c61cba6d5b106d
JorgeDeLira <e-jedelira@neoris.com>
2025-04-29 13:07:56 -0700

    2

--- P_05_Uni_Deferred.java
+++ P_05_Uni_Deferred.java
@@ -1,7 +1,20 @@
 package P_01_basics;
 
+import io.smallrye.mutiny.Uni;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 public class P_05_Uni_Deferred {
     public static void main(String[] args) {
+        System.out.println(" Deferred Uni");
+
+        AtomicLong ids = new AtomicLong();
+
+        Uni<Long> deferredUni = Uni.createFrom().deferred(() -> Uni.createFrom().item(ids::incrementAndGet));
+
+        for (var i = 0; i < 5; i++){
+            deferredUni.subscribe().with(System.out::println);
+        }
 
     }
 }
