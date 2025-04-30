462383591ca0a3cacd2a0b22d8dec13bfd12f20b
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-04-30 10:44:43 -0700

    7

--- P_10_Uni_Misc.java
+++ P_10_Uni_Misc.java
@@ -1,4 +1,28 @@
 package P_01_basics;
 
+import io.smallrye.mutiny.Uni;
+
+import java.util.Optional;
+
 public class P_10_Uni_Misc {
+    public static void main(String[] args) {
+        System.out.println("Misc");
+
+        Uni.createFrom().nothing()
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+        Uni.createFrom().voidItem()
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+        Uni.createFrom().nullItem()
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+        Uni.createFrom().optional(Optional.of("Hello"))
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+        Uni.createFrom().converter(i -> Uni.createFrom().item("[" + i + "]"), 10)
+                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+
+
+    }
 }
