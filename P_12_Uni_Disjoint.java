494fc8568371e685d775955da38709f1c0e0e5e8
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-05-02 15:56:32 -0700

    5

--- P_12_Uni_Disjoint.java
+++ P_12_Uni_Disjoint.java
@@ -1,4 +1,16 @@
 package P_01_basics;
 
+i
+mport io.smallrye.mutiny.Uni;
+
+import java.util.List;
+
 public class P_12_Uni_Disjoint {
+    public static void main(String[] args) {
+        System.out.println("Uni disjoint");
+
+        Uni.createFrom().item(List.of(1, 2, 3, 4, 5))
+                .onItem().disjoint()
+                .subscribe().with(System.out::println);
+    }
 }
