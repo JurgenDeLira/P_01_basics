f3439f05c1c6340ea627e71dc6eeaca2bd9ed860
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-05-13 15:27:53 -0700

    10

--- P_14_Multi_Subscriber.java
+++ P_14_Multi_Subscriber.java
@@ -1,4 +1,41 @@
 package P_01_basics;
 
+import io.smallrye.mutiny.Multi;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
 public class P_14_Multi_Subscriber {
+
+    public static void main(String[] args) {
+        System.out.println("⚡️ Hello world with subscriber");
+
+        Multi.createFrom().items(1, 2, 3).subscribe().withSubscriber(new Subscriber<Integer>() {
+            private Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println("onSubscribe()");
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext: " + integer);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("onError: " + t.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete()");
+            }
+        });
+    }
 }
